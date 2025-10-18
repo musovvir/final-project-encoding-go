4f3c04a841d391a12000444d0e929f231e82bae6
musovvir <mr.muzagov@mail.ru>
2025-10-18 23:52:17 +0300

    реализовал методы Encoding для JSONData и YAMLData

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,12 @@
 package encoding
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/hive-bootcamp/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,8 +30,32 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+	// 1 Читаю файл json
+	data, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		return fmt.Errorf("ошибка при чтении файла: %s", err.Error())
+	}
+
+	// 2 Вытаскиваю json данные
+	var jsonData interface{}
+	err = json.Unmarshal(data, &jsonData)
+	if err != nil {
+		return fmt.Errorf("ошибка при парсинге JSON: %s", err.Error())
+	}
+
+	// 3 Из данных json делаю yaml
+	yamlData, err := yaml.Marshal(jsonData)
+
+	if err != nil {
+		return fmt.Errorf("ошибка при попытке маршалирования: %s", err.Error())
+	}
+
+	// 4 Записаваю yaml данные в файл yaml
+
+	err = os.WriteFile(j.FileOutput, yamlData, 0644)
+	if err != nil {
+		return fmt.Errorf("ошибка при записи данных: %s", err.Error())
+	}
 
 	return nil
 }
@@ -34,7 +63,31 @@ func (j *JSONData) Encoding() error {
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
 	// Ниже реализуйте метод
-	// ...
+	// 1 Читаю файл yaml
+	data, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		return fmt.Errorf("ошибка при чтении файла: %s", err.Error())
+	}
+
+	// 2 Вытаскиваю yaml данные
+	var yamlData interface{}
+
+	err = yaml.Unmarshal(data, &yamlData)
+	if err != nil {
+		return fmt.Errorf("ошибка при чтении данных yaml: %s", err.Error())
+	}
+
+	// 3 Из данных yaml делаю json
+	jsonData, err := json.MarshalIndent(yamlData, "", "    ")
+	if err != nil {
+		return fmt.Errorf("ошибка при попытке маршалирования: %s", err.Error())
+	}
+
+	// 4 Записаваю json данные в файл json
+	err = os.WriteFile(y.FileOutput, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("ошибка при записи данных: %s", err.Error())
+	}
 
 	return nil
 }
